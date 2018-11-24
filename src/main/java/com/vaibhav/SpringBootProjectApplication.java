01951b6000c6b27e71c0f7c9b1a0e5a3bb1e5b02
“vaibhav30github” <vaibhav.verma90@hotmail.com>
2018-11-25 02:20:46 +0530

    Basic authentication implementation

--- src/main/java/com/vaibhav/SpringBootProjectApplication.java
+++ src/main/java/com/vaibhav/SpringBootProjectApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableWebSecurity
 @EnableSwagger2
 public class SpringBootProjectApplication extends SpringBootServletInitializer {
 
